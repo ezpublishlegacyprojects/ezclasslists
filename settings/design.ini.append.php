943f851b0361a28584916d43abf0a82723f0126f
Damien Pobel <damien@pobel.fr>
2008-11-10 16:24:05 +0000

    Remove no more used stylesheet
    git-svn-id: http://svn.projects.ez.no/ezclasslists@6 aa5136f7-8a9a-4caa-80bb-068c1e46dfeb

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -3,9 +3,6 @@
 [ExtensionSettings]
 DesignExtensions[]=ezclasslists
 
-[StylesheetSettings]
-CSSFileList[]=classlists.css
-
 [JavaScriptSettings]
 JavaScriptList[]=yui/2.5.2/build/utilities/utilities.js
 JavaScriptList[]=classlists.js
