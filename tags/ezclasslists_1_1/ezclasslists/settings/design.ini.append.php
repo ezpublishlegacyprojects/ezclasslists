ad220712472b63e8e86e743cf70c9def151ba53e
Damien Pobel <damien@pobel.fr>
2009-11-11 21:26:30 +0000

    Created tag for 1.1 release
    git-svn-id: http://svn.projects.ez.no/ezclasslists@13 aa5136f7-8a9a-4caa-80bb-068c1e46dfeb

---
+++ tags/ezclasslists_1_1/ezclasslists/settings/design.ini.append.php
@@ -0,0 +1,10 @@
+<?php /*
+
+[ExtensionSettings]
+DesignExtensions[]=ezclasslists
+
+[JavaScriptSettings]
+JavaScriptList[]=yui/2.5.2/build/utilities/utilities.js
+JavaScriptList[]=classlists.js
+
+*/ ?>
---
+++ tags/ezclasslists_1_1/ezclasslists/settings/lists.ini.append.php
@@ -0,0 +1,30 @@
+<?php /*
+
+[DeleteSettings]
+ConfirmJavascript=enabled
+DefaultMoveToTrash=enabled
+
+[ListSettings]
+# DefaultSortMethod should be one of the following values : 
+#  - depth
+#  - modified
+#  - name
+#  - path
+#  - path_string
+#  - priority
+#  - published
+#  - section
+DefaultSortMethod=modified
+
+# ascending or descending
+DefaultSortOrder=descending
+
+IncludeClasses[]
+# Fill this array with class identifier to have only
+# those class listed in the left menu
+#IncludeClasses[]=folder
+#IncludeClasses[]=image
+
+
+
+*/ ?>
